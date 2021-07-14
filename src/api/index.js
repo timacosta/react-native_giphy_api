7453410dfaccf331f7b9b0366a9dc72f1b38fed1
Tim Acosta <timacosta06@gmail.com>
2021-07-14 17:53:40 +0100

    axios configuration

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import {API_URL, API_KEY} from '../config/api'
+
+const axiosInstance = axios.create({
+    baseUrl: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        'Api-Key': API_KEY
+    }
+});
