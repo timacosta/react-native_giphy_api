633af3722374a68f89c8e2c8ff972daa5725dd5e
Tim Acosta <timacosta06@gmail.com>
2021-07-16 19:05:18 +0100

    redux-thunk added

--- src/components/pages/system/app/index.js
+++ src/components/pages/system/app/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {Router, Stack, Scene, Actions} from 'react-native-router-flux'
+import {Provider} from 'react-redux'
 
 import Home from '../../home/view';
 import Details from '../../details/view';
-
+import store from '../../../../config/redux';
 
 
 
@@ -11,12 +12,15 @@ import Details from '../../details/view';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Provider store={store}>
+        <Router>
         <Stack key="root">
           <Scene key="Home" component={Home} hideNavBar initial ></Scene>
           <Scene key="Details" component={Details}/>
         </Stack>
       </Router>
+      </Provider>
+      
     );
   }
 };
